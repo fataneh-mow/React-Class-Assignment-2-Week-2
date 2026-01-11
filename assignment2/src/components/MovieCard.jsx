0dd3f964139cafb3da7c06487a5dd096785ff26a
fataneh-mow <fataneh.mow@gmail.com>
2026-01-11 13:25:05 +0430

    MovieCard component created

--- assignment2/src/components/MovieCard.jsx
+++ assignment2/src/components/MovieCard.jsx
@@ -1,9 +1,44 @@
 import { useState } from "react";
 
+import { TbSquareRounded } from "react-icons/tb";
+import { TbSquareRoundedCheckFilled } from "react-icons/tb";
+import { MdOutlineDelete } from "react-icons/md";
+
+
+
 export default function MovieCard ({title, info}) {
+    const [watched, setWatched] = useState(false)
+
+
+   function HandleWatchMovie () {
+    setWatched(prev => !prev)
+   }
+
     return (
-        <div>
-            
+        <div className="bg-red-950 mx-auto w-1/2 rounded-md shadow-lg py-4 flex justify-between">
+            <div className="flex items-center px-4">
+                <button
+                    onClick={HandleWatchMovie}
+                >
+                    {
+                        watched ? (
+                            <TbSquareRoundedCheckFilled className="text-red-500" size={24}/>
+                        ) : (
+                            <TbSquareRounded className="text-red-500" size={24}/>
+                        )
+                    }
+                </button>
+                <div className="px-4">
+                    <h1 className="text-red-500 text-xl font-semibold">{title}</h1>
+                    <p className="text-red-500 text-md">{info}</p>
+                </div>
+            </div>
+            <div className="mt-3 px-4">
+                <button>
+                    <MdOutlineDelete className="text-red-500" size={24} />
+                </button>
+            </div>
+           
         </div>
     )
 }
