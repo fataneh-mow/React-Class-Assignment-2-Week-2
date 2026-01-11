f347b49770253b724de478fc48ab461ff3094040
fataneh-mow <fataneh.mow@gmail.com>
2026-01-11 13:25:30 +0430

    MovieCard component created

--- assignment2/src/App.jsx
+++ assignment2/src/App.jsx
@@ -1,1 +1,10 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
+
+export default function App () {
+  return (
+    <div className='my-12'>
+      <MovieCard title={"Strange Things"} info={"This film was made perfectly"}></MovieCard>
+    </div>
+  )
+}
