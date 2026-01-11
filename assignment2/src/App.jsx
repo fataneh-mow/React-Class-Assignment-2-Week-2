75b592b0bc970eed2d76e54485d427be63ce0dd5
fataneh-mow <fataneh.mow@gmail.com>
2026-01-11 13:44:52 +0430

    Header component implemented

--- assignment2/src/App.jsx
+++ assignment2/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Header from './components/Header'
 
 export default function App () {
   return (
-    <div className='my-12'>
+    <div className='my-6'>
+      <Header title={"My Movie List"} subTitle={"A React Application for movie watch list managment!"}></Header>
       <MovieCard title={"Strange Things"} info={"This film was made perfectly"}></MovieCard>
     </div>
   )
