b4c861e189c0880d0d6ee837f357b5f8bbb9ecb7
fataneh-mow <fataneh.mow@gmail.com>
2026-01-13 20:11:13 +0430

    fix: new features implemented and problems fixed

--- assignment2/src/components/AddMovie.jsx
+++ assignment2/src/components/AddMovie.jsx
@@ -1,7 +1,31 @@
 import { useState } from "react"
 
-export default function AddMovie () {
-    const [isFormOpen, setFormOpen] = useState(false)
+export default function AddMovie ({onAddMovie}) {
+    const [isFormOpen, setFormOpen] = useState(false);
+    const [title, setTitle] = useState('');
+    const [genre, setGenre] = useState('');
+    const [info, setInfo] = useState('')
+
+    function createId() {
+        if (typeof crypto !== "undefined" && crypto.randomUUID) return crypto.randomUUID();
+        return `${Date.now()}-${Math.random().toString(16).slice(2)}`;
+    }
+
+    function HandleSubmit (e) {
+        e.preventDefault();
+
+        onAddMovie({
+            id: createId(),
+            title,
+            genre,
+            info
+        });
+
+        setTitle("");
+        setGenre("");
+        setInfo("")
+        setFormOpen(false);
+    }
 
     return (
         <div className="">
@@ -16,47 +40,72 @@ export default function AddMovie () {
             </button>
             <div>
                 {
-                    !isFormOpen &&
-                    <div className="flex justify-center mx-auto w-1/2  ">
-                        <div className="bg-red-100 rounded-md m-12">
-                            <form className="px-8 py-4">
-                                <h1 className="text-2xl text-red-800 mb-2">New Movie</h1>
-                                <hr />
-                                <div className="my-4 grid gap-4">
-                                    <div>
-                                        <label htmlFor="movie" className="text-red-500 text-lg">Movie Title</label>
-                                        <input type="text" placeholder="Movie..." 
-                                            className="bg-red-50 rounded-md shadow-md p-1 outline-none text-gray-400" 
-                                        />
-                                    </div>
-                                    
-                                    <div>
-                                        <label htmlFor="genre" className="text-red-500 text-lg">Genre</label>
+                    isFormOpen && (
+                        <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+                            <div className="bg-red-100 rounded-md m-12 w-[90%] max-w-md shadow-xl relative">
+                                <form className="px-8 py-4" onSubmit={HandleSubmit}>
+                                    <span className="grid grid-cols-2 justify-between">
+                                        <h1 className="text-2xl text-red-800 mb-2">New Movie</h1>
+                                        <button 
+                                            onClick={() => setFormOpen(!isFormOpen)}
+                                            className="text-sm ml-auto mb-1"
+                                        >
+                                            ❌
+                                        </button>
+                                    </span>
+                                    <hr />
+                                    <div className="my-4 grid gap-4">
                                         <div className="grid grid-rows-2">
-                                                <select name="genre" id="genre"
-                                                    className="bg-red-50 rounded-md custom-select-gray text-gray-400 outline-none appearance-none shadow-md"
-                                                >
-                                                    <option value="daram">Daram</option>
-                                                    <option value="comedy">Comedy</option>
-                                                    <option value="romance">Romance</option>
-                                                    <option value="frightening">Frightening</option>
-                                                    <option value="daram">Daram</option>
-                                                    <option value="daram">Daram</option>
-
-                                                </select>
+                                            <label htmlFor="movie" className="text-red-500 text-lg">Movie Title</label>
+                                            <input 
+                                                type="text" placeholder="Movie..."
+                                                onChange={e => setTitle(e.target.value)} 
+                                                required
+                                                className="bg-red-50 rounded-md shadow-md p-1 outline-none text-gray-400" 
+                                            />
                                         </div>
-                                    </div>
+                                        
+                                        <div className="grid grid-rows-2">
+                                            <label htmlFor="info" className="text-red-500 text-lg">Information</label>
+                                            <input 
+                                                type="text" placeholder="Info..."
+                                                onChange={e => setInfo(e.target.value)} 
+                                                required
+                                                className="bg-red-50 rounded-md shadow-md p-1 outline-none text-gray-400" 
+                                            />
+                                        </div>
 
-                                    <button 
-                                        className="bg-red-800 text-red-50 rounded-md shadow-md py-1 hover:bg-red-900 transition ease-in "
-                                    >
-                                        Save
-                                    </button>
-                                    
-                                </div>
-                            </form>
+                                        <div>
+                                            <label htmlFor="genre" className="text-red-500 text-lg">Genre</label>
+                                            <div className="grid grid-rows-2">
+                                                    <select 
+                                                        name="genre" id="genre"
+                                                        className="w-full bg-red-50 rounded-md custom-select-gray text-gray-400 outline-none appearance-none shadow-md"
+                                                        onChange={e => setGenre(e.target.value)} 
+                                                        required
+                                                    >
+                                                        <option>Drama</option>
+                                                        <option>Comedy</option>
+                                                        <option>Romance</option>
+                                                        <option>Action</option>
+                                                        <option>Thriller</option>
+                                                    </select>
+                                            </div>
+                                        </div>
+
+                                        <button 
+                                            className="bg-red-800 text-red-50 rounded-md shadow-md py-1 hover:bg-red-900 transition ease-in "
+                                            type="submit"
+                                        >
+                                            Save
+                                        </button>
+                                        
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                    </div>
+                    )
+                    
                 }
             </div>
         </div>
