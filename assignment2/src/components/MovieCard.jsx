95fb35f81fad64b300ac6ad2a629fbbd79df0b61
fataneh-mow <fataneh.mow@gmail.com>
2026-01-13 13:30:37 +0430

    feat: movie card improved

--- assignment2/src/components/MovieCard.jsx
+++ assignment2/src/components/MovieCard.jsx
@@ -8,34 +8,40 @@ import { MdOutlineDelete } from "react-icons/md";
 
 export default function MovieCard ({title, info}) {
     const [watched, setWatched] = useState(false)
-
+    const [deleted, setDeleted] = useState(false)
 
    function HandleWatchMovie () {
     setWatched(prev => !prev)
    }
 
+   function HandleDelete () {
+    setDeleted(!deleted)
+   }
+
     return (
-        <div className="bg-red-950 mx-auto w-1/2 py-2 rounded-md shadow-lg flex justify-between">
+        <div className="bg-red-100 mx-auto w-1/2 py-2 rounded-md shadow-lg flex justify-between">
             <div className="flex items-center px-6">
                 <button
                     onClick={HandleWatchMovie}
                 >
                     {
                         watched ? (
-                            <TbSquareRoundedCheckFilled className="text-red-500" size={24}/>
+                            <TbSquareRoundedCheckFilled className="text-red-800" size={24}/>
                         ) : (
-                            <TbSquareRounded className="text-red-500" size={24}/>
+                            <TbSquareRounded className="text-red-800" size={24}/>
                         )
                     }
                 </button>
                 <div className="px-4">
-                    <h1 className="text-red-500 text-xl">{title}</h1>
-                    <p className="text-red-500 text-md">{info}</p>
+                    <h1 className="text-red-800 text-xl">{title}</h1>
+                    <p className="text-red-800 text-md">{info}</p>
                 </div>
             </div>
             <div className="mt-3 px-5">
-                <button>
-                    <MdOutlineDelete className="text-red-500" size={20} />
+                <button
+                    onClick={HandleDelete}
+                >
+                    <MdOutlineDelete className="text-red-800" size={20} />
                 </button>
             </div>
            
