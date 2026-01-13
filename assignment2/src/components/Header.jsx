18839f901656da7ec603293bdce2c2ae2e79fc87
fataneh-mow <fataneh.mow@gmail.com>
2026-01-13 13:30:54 +0430

    style: style improved

--- assignment2/src/components/Header.jsx
+++ assignment2/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header ({title, subTitle}) {
     return (
-        <div className=" mx-auto w-1/2 rounded-md shadow-lg py-4 text-center text-red-800 my-8">
+        <div className=" mx-auto w-1/2 rounded-md shadow-lg py-4 text-center text-red-100 my-8">
             <h1 className="font-semibold text-3xl">{title}</h1>
             <p>{subTitle}</p>
         </div>
