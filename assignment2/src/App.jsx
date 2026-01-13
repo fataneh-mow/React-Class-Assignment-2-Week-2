5712301968389e52c134fcded8abe8cf7b644f0b
fataneh-mow <fataneh.mow@gmail.com>
2026-01-13 13:32:06 +0430

    feat: new componnets created

--- assignment2/src/App.jsx
+++ assignment2/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 import Header from './components/Header'
 import FilterMovies from './components/FilterMovies'
+import AddMovie from './components/AddMovie'
 
 export default function App () {
   return (
@@ -9,8 +10,13 @@ export default function App () {
       <div>
         <Header title={"My Movie List"} subTitle={"A React Application for movie watch list managment!"}></Header>
       </div>
+      <div className='grid grid-cols-2 justify-between mx-auto w-1/2 my-4'>
+        <AddMovie />
+        <span className='ml-auto'>
+          <FilterMovies />
+        </span>
+      </div>
       <div className='grid grid-rows-2'>
-        <FilterMovies />
         <MovieCard title={"Strange Things"} info={"This film was made perfectly"}></MovieCard>
       </div>
     </div>
