468c17418e0af0e23196a901f979632482e09924
fataneh-mow <fataneh.mow@gmail.com>
2026-01-14 22:14:16 +0430

    feat: new fweatures and functionalities implemeted

--- assignment2/src/components/MovieCard.jsx
+++ assignment2/src/components/MovieCard.jsx
@@ -6,16 +6,10 @@ import { MdOutlineDelete } from "react-icons/md";
 
 
 
-export default function MovieCard ({id, title, info, genre, onDelete}) {
-    const [watched, setWatched] = useState(false)
-
-   function HandleWatchMovie () {
-    setWatched(true)
-   }
-
-   function HandleDeleteMovie () {
-    onDelete(id)
-   }
+export default function MovieCard ({id, title, info, genre ,watched ,onDelete, onWatch}) {
+    function HandleWatchMovie () {
+        if (!watched) onWatch(id);
+    }
 
     return (
         <div className="bg-red-100 mx-auto w-1/2 py-4 rounded-md shadow-lg flex justify-between">
@@ -36,8 +30,11 @@ export default function MovieCard ({id, title, info, genre, onDelete}) {
                     <div>
                         <span className="flex justify-between gap-4">
                             <p className="text-red-800 text-md">{info || "No information"}</p>
-                            <span className="bg-red-50 text-red-800 px-4 rounded-lg">
+                            <span className="bg-red-200 text-red-800 px-4 rounded-lg">
                                 {genre || "No genre"}
+                            </span>
+                            <span className="bg-red-200 text-red-800 px-4 rounded-lg">
+                                {watched ? "Watched" : "Unwatched"}
                             </span>
                         </span>
                     </div>
@@ -45,7 +42,7 @@ export default function MovieCard ({id, title, info, genre, onDelete}) {
             </div>
             <div className="mt-3 px-5">
                 <button
-                    onClick={HandleDeleteMovie}
+                    onClick={() => onDelete(id)}
                 >
                     <MdOutlineDelete className="text-red-800" size={20} />
                 </button>
