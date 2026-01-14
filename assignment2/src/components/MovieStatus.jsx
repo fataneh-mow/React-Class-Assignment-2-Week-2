bf0f581405ed7765fcf0ce09f7d038e3cccbab61
fataneh-mow <fataneh.mow@gmail.com>
2026-01-14 20:04:03 +0430

    feat: MovieStatus componnet UI created

---
+++ assignment2/src/components/MovieStatus.jsx
@@ -0,0 +1,35 @@
+import { RiMovie2Fill } from "react-icons/ri";
+import { CiNoWaitingSign } from "react-icons/ci";
+import { BiSolidMoviePlay } from "react-icons/bi";
+
+
+export default function MovieStatus ({wathcedCount, unwathcedCount, totalCount}) {
+    return (
+        <div className="bg-red-100 w-1/2 text-red-800 mx-auto rounded-md shadow-lg p-4">
+            <h1 className="text-center text-2xl font-bold my-2 mb-4">Latest Updates</h1>
+            <div className="grid grid-cols-3 gap-3 justify-center">
+                <div className="bg-red-200 rounded-md p-4">
+                    <h2 className="text-xl font-semibold justify-center flex items-center">
+                        <RiMovie2Fill size={20} className="mr-1 mb-[0.1rem]" />
+                        Watched
+                    </h2>
+                    <p className="text-center mt-3">{wathcedCount} Movies</p>
+                </div>
+                <div className="bg-red-50 rounded-md p-4">
+                    <h2 className="text-xl font-semibold justify-center flex items-center">
+                        <CiNoWaitingSign size={20} className="mr-1 mb-[0.1rem]" />
+                        Unwatched
+                    </h2>
+                    <p className="text-center mt-3">{unwathcedCount} Movies</p>
+                </div>
+                <div className="bg-red-200 rounded-md p-4">
+                    <h2 className="text-xl font-semibold justify-center flex items-center">
+                        <BiSolidMoviePlay size={20} className="mr-1 mb-[0.1rem]" />
+                        Total Movies
+                    </h2>
+                    <p className="text-center mt-3">{wathcedCount} Movies</p>
+                </div>    
+            </div>
+        </div>
+    )
+}
