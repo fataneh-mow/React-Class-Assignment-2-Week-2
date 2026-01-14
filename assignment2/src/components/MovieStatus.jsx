cf8065ee7857865277120a74e2647c6915529353
fataneh-mow <fataneh.mow@gmail.com>
2026-01-14 22:13:09 +0430

    feat: MovieStatus functionality created and necessary props with correct spelling passed

--- assignment2/src/components/MovieStatus.jsx
+++ assignment2/src/components/MovieStatus.jsx
@@ -3,7 +3,7 @@ import { CiNoWaitingSign } from "react-icons/ci";
 import { BiSolidMoviePlay } from "react-icons/bi";
 
 
-export default function MovieStatus ({wathcedCount, unwathcedCount, totalCount}) {
+export default function MovieStatus ({watchedCount, unwathcedCount, totalCount}) {
     return (
         <div className="bg-red-100 w-1/2 text-red-800 mx-auto rounded-md shadow-lg p-4">
             <h1 className="text-center text-2xl font-bold my-2 mb-4">Latest Updates</h1>
@@ -13,7 +13,7 @@ export default function MovieStatus ({wathcedCount, unwathcedCount, totalCount})
                         <RiMovie2Fill size={20} className="mr-1 mb-[0.1rem]" />
                         Watched
                     </h2>
-                    <p className="text-center mt-3">{wathcedCount} Movies</p>
+                    <p className="text-center mt-3">{watchedCount} Movies</p>
                 </div>
                 <div className="bg-red-50 rounded-md p-4">
                     <h2 className="text-xl font-semibold justify-center flex items-center">
@@ -27,7 +27,7 @@ export default function MovieStatus ({wathcedCount, unwathcedCount, totalCount})
                         <BiSolidMoviePlay size={20} className="mr-1 mb-[0.1rem]" />
                         Total Movies
                     </h2>
-                    <p className="text-center mt-3">{wathcedCount} Movies</p>
+                    <p className="text-center mt-3">{totalCount} Movies</p>
                 </div>    
             </div>
         </div>
