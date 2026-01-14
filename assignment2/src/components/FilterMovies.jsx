d0e92f524486c67218e79ea2d16b8625cfa4bf21
fataneh-mow <fataneh.mow@gmail.com>
2026-01-14 22:12:02 +0430

    feat: FilterMovie functionality added, necessary props passed

--- assignment2/src/components/FilterMovies.jsx
+++ assignment2/src/components/FilterMovies.jsx
@@ -1,9 +1,12 @@
-export default function FilterMovies () {
+export default function FilterMovies ({filter, setFilter}) {
     return (
         <div className="">
             <form className="bg-red-100 rounded-md py-[0.1rem]">
-                <select name="filter" defaultValue="All" 
+                <select 
+                    name="filter" 
+                    value={filter} 
                     className="bg-red-100 rounded-md custom-select outline-none appearance-none"
+                    onChange={(e) => {setFilter(e.target.value)}}
                 >
                     <option value="All">All</option>
                     <option value="watched">Watched</option>
